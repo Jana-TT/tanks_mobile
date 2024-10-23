5d16e87475dc55aa11cdcaea444e08f88b5566aa
Jana-TT <janatt2056@gmail.com>
2024-10-23 11:54:10 -0500

    Initial Commit

---
+++ iTanks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTanks
+//
+//  Created by Jana Tahan  on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTanks/iTanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTanksApp.swift
+//  iTanks
+//
+//  Created by Jana Tahan  on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct iTanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
