2ad0f6d1870d3841b71db111772a6dc5be39d762
Jana-TT <janatt2056@gmail.com>
2024-10-24 13:56:36 -0500

    tank card progress

--- components/tank_card.swift
+++ components/tank_card.swift
@@ -7,14 +7,62 @@
 
 import SwiftUI
 
+func inches_to_feet(inches: Float?) -> String {
+    guard let inches = inches else {
+        return "" }
+    
+    var feet = inches / 12
+    feet = round(feet * 10) / 10
+    let feet_string = String(format: "%.1f", feet)
+    let parts = feet_string.split(separator: ".")
+    
+    let feet_part = parts[0]
+    let inch_part = parts[1]
+    
+    return "\(feet_part)'" + "\(inch_part)\""
+}
+
 struct TankCardView: View {
-    //passing in the tank data
     let tank: Tank
     
     var body: some View {
-        VStack{
+        HStack {
             PercentFullView(tank: tank)
+            
+            VStack(alignment: .leading) {
+                Text("\(tank.tank_type) Tank #\(tank.tank_number)")
+                    .foregroundColor(tank.tank_type == "Oil" ? Color.green : Color.blue)
+                    .bold()
+            
+                if let inchesToESD = tank.inches_to_esd {
+                    Text("\(inches_to_feet(inches: inchesToESD)) to ESD")
+                        .padding(6)
+                        .font(.system(size: 14))
+                        .bold()
+                        .foregroundColor(.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue.opacity(0.5))
+                        )
+                } else {
+                    Text("")
+                }
+            }
+            
+            Spacer()
+            
+            HStack(spacing: 30) {
+                VStack {
+                    Text("\(inches_to_feet(inches: tank.level))")
+                    Text("Level")
+                }
+                VStack {
+                    Text("\(tank.capacity)")
+                    Text("BBL")
+                }
+            }
         }
     }
 }
+
 
